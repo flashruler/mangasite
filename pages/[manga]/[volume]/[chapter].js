dcc3cf8ea9140b7a074904fbf1f15f84ee205612
flashruler <44584657+flashruler@users.noreply.github.com>
2021-07-09 22:03:35 -0700

    Added go and back button
    Its hardcoded which needs to change

--- pages/[manga]/[volume]/[chapter].js
+++ pages/[manga]/[volume]/[chapter].js
@@ -1,20 +1,33 @@
 import { useRouter } from 'next/router';
-import React, { Component } from "react";
+import React, {  useState  } from "react";
+
 // import Page from '../components/Page';
 
 //import Chapter from '../components/chapter';
 //manga.com/[name]/[volume]/[chapter]
 //smartphone.volume1.c1
 function Chapter(props) {
-    console.log(props.chapterImages)
+    const [count, setCount] = useState(0);
+
     return (
-        
+
         <div>
-            {props.chapterImages.map((image, index) => {
-                return <img src={props.chapterImages} key={index}></img>
-            })}
-            
+            {
+            props.chapterImages.map((image, index) => {
+                return (
+                    null) })}
+                    <img src={props.chapterImages[count]}></img>
+                    <button onClick={() => {if(count !=32){
+                        setCount(count + 1)}}}>
+                        Next
+                    </button>
+                    <button onClick={() => {if(count !=0){
+                        setCount(count - 1)}}}>
+                        Back
+                    </button>
+
         </div>
+        
     );
 }
 export async function getStaticPaths() {
@@ -23,26 +36,26 @@ export async function getStaticPaths() {
     let mangaList = await import("../../../public/mangas.json");
     mangaList = mangaList.mangas
 
-    for(let i = 0; i<mangaList.length; i++) {
+    for (let i = 0; i < mangaList.length; i++) {
         data[mangaList[i]] = await import("../../../public/" + mangaList[i] + ".json");
     }
 
     const paths = []
-    for(let i = 0; i<mangaList.length; i++) {
-        for(let k = 0; k<data[mangaList].volumes.length; k++) {
-            for(let j = 0; j<data[mangaList].volumes[k].chapters.length; j++) {
-                paths.push("/"+mangaList[i]+"/volume"+data[mangaList[i]].volumes[k].volumeNumber+"/c"+data[mangaList[i]].volumes[k].chapters[j].chapterNumber)
+    for (let i = 0; i < mangaList.length; i++) {
+        for (let k = 0; k < data[mangaList].volumes.length; k++) {
+            for (let j = 0; j < data[mangaList].volumes[k].chapters.length; j++) {
+                paths.push("/" + mangaList[i] + "/volume" + data[mangaList[i]].volumes[k].volumeNumber + "/c" + data[mangaList[i]].volumes[k].chapters[j].chapterNumber)
             }
         }
     }
 
     console.log("test")
-    
+
 
     return { paths: paths, fallback: true }
-  }
+}
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
     // const router = useRouter();
     // const c = router.query.chapter;
     // const volume = router.query.volume;
@@ -53,23 +66,23 @@ export async function getStaticProps({params}) {
     let c = params.chapter;
     let volume = params.volume;
     let manga = params.manga;
-    c = Number(c.replace("c",""));
-    volume = Number(volume.replace("volume",""));
+    c = Number(c.replace("c", ""));
+    volume = Number(volume.replace("volume", ""));
     if (manga) {
         let data = await import("../../../public/" + manga + ".json");
         data = data.volumes
         if (data) {
             let volumeIndex = -1;
             let chapterImages = []
-            for(let i = 0; i<data.length; i++) {
-                if(data[i].volumeNumber = volume) {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].volumeNumber = volume) {
                     volumeIndex = i;
                 }
             }
-            for(let i = 0; i<data[volumeIndex].chapters.length; i++) {
-                if(data[volumeIndex].chapters[i].chapterNumber = c) {
+            for (let i = 0; i < data[volumeIndex].chapters.length; i++) {
+                if (data[volumeIndex].chapters[i].chapterNumber = c) {
                     chapterImages = data[volumeIndex].chapters[i].images;
-                    return { props: { chapterImages: chapterImages} };
+                    return { props: { chapterImages: chapterImages } };
                 }
             }
         }
