9b0dbaccb7a3fdcddc93ea936fa330069539b35d
flashruler <44584657+flashruler@users.noreply.github.com>
2021-07-20 17:45:51 -0700

    updated header.js

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,19 @@
 import styles from './header.module.css'
 import Link from 'next/link';
-function Header(){
-return(
-<div className={styles.containerHeader}>
-    <div>
-        <img src= "https://i.ibb.co/ZMj9gXT/TLS-LOGO.png"></img>
-    </div>
-</div>
-);
+function Header() {
+    return (
+        <div className={styles.containerHeader}>
+            <div>
+                <Link href='/'><a><img src="https://i.ibb.co/ZMj9gXT/TLS-LOGO.png"></img></a></Link>
+            </div>
+            <nav>
+                    <ul className={styles.links}>
+                        <li><Link href='#'><a className={styles.headerTitle}>About Us</a></Link></li>
+                        <li><Link href='#'><a className={styles.headerTitle}>FAQ</a></Link></li>
+                        <li><Link href='#'><a className={styles.headerTitle}>Donate</a></Link></li>
+                    </ul>
+            </nav>
+        </div>
+    );
 }
 export default Header;
