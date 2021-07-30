cd983517f59c7309a4ebaef7dfb80ca51bb270db
flashruler <44584657+flashruler@users.noreply.github.com>
2021-07-29 22:53:16 -0700

    CSS Work on Volume page and optimized for Mobile/Ipad

--- pages/[manga]/[volume]/index.js
+++ pages/[manga]/[volume]/index.js
@@ -1,17 +1,28 @@
 import Link from 'next/link';
 import { Fragment } from 'react';
+import Header from '../../../components/Header';
 function Volume(props) {
     console.log(props)
-    if(props.chapterList){
+    if (props.chapterList) {
         return <Fragment>
-        <h1>{"Volume "+props.volume}</h1>
-        <ul>
-            {props.chapterList.map(chapter=>
-            <li key={chapter}><Link href={"/"+props.manga+"/volume"+props.volume+"/c"+chapter}>{"chapter "+chapter}</Link></li>)}
-        </ul>
-    </Fragment>
+            <Header />
+            <h1 className="text-5xl my-4 text-white bg-transparent font-extralight text-center uppercase sm:text-4xl md:text-5xl lg:text-left lg:mx-4">{"Volume " + props.volume}</h1>
+            <ul className=" justify-center mx-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:grid-gap-3 2xl:grid-cols-8 xl:grid-cols-6">
+                <li>
+                    <div className="opacity-50"><Link href={"/"+props.manga}><span><svg xmlns="http://www.w3.org/2000/svg" className="h-40 w-40 mx-auto" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M11 17l-5-5m0 0l5-5m-5 5h12" /></svg>
+                    <h1 className="text-center text-white uppercase text-3xl font-light">back</h1></span></Link>
+                    </div>
+                </li>
+                {props.chapterList && props.chapterList.map(chapter =>
+                    <li key={chapter.chapterNumber}><Link href={"/" + props.manga + "/volume" + props.volume + "/" + chapter.chapterNumber}>
+                        <span>
+                            <img src={chapter.chapterCover} className="max-h-96 w-auto rounded-2xl p-0 object-contain lg:mx-auto">
+                            </img>
+                        </span></Link></li>)}
+            </ul>
+        </Fragment>
     }
-    return(null)
+    return (null)
 
 }
 export async function getStaticPaths() {
@@ -50,12 +61,16 @@ export async function getStaticProps({ params }) {
                 }
             }
             for (let i = 0; i < data[volumeIndex].chapters.length; i++) { //iterate through each chapter
-                chapterList.push(data[volumeIndex].chapters[i].chapterNumber) //pushes chapter list of volume
+                chapterList.push(data[volumeIndex].chapters[i]) //pushes chapter list of volume
 
             }
-            return { props: { chapterList: chapterList,
-             volume: volume,
-            manga: manga} };
+            return {
+                props: {
+                    chapterList: chapterList,
+                    volume: volume,
+                    manga: manga
+                }
+            };
         }
     }
 }
--- pages/[manga]/index.js
+++ pages/[manga]/index.js
@@ -5,26 +5,30 @@ import Header from '../../components/Header';
 function Manga(props) {
     return <div>
         <Header />
-        <Fragment>
-            <div className="flex flex-col justify-center mx-auto my-6 rounded-3xl">
-                <div className="flex w-full max-h-60"><img src={props.volumeList[0].imageCover} className="object-center self-end mx-auto object-cover h-48 w-full"></img></div>
-                <div className="flex-box justify-center self-end bg-isesuma-darkpurple max-h-80 overflow-auto">
-                    <h1 className="text-6xl my-2 text-white bg-transparent font-extralight text-center uppercase sm:text-4xl">{props.manga}</h1>
-                    <h4 className="text-sm text-white bg-transparent mx-4">{props.description}</h4>
-                    <ul className=" flex flex-row bg-transparent justify-center">
+        <div>
+            <div className="flex flex-col justify-center mx-auto my-6 rounded-3xl sm:flex-row md:h-80 lg:bg-isesuma-purple">
+                <img src={props.volumeList[0].imageCover} className="max-h-80 sm:w-full object-center self-end object-cover h-48 sm:h-60 w-full sm:object-contain md:h-80 lg:max-h-96 lg:object-cover xl:object-contain 2xl:object-contain bg-isesuma-darkpurple "></img>
+                <div className="flex-box justify-center bg-isesuma-darkpurple max-h-80 overflow-auto sm:max-h-60 md:max-h-80 lg:h-96">
+                    <h1 className="text-5xl my-1 text-white bg-transparent font-extralight text-center uppercase sm:text-4xl md:text-5xl lg:text-left lg:mx-4">{props.manga}</h1>
+                    <h4 className="text-sm text-white bg-transparent mx-4 lg:text-base">{props.description}</h4>
+                    <ul className=" flex flex-row bg-transparent justify-center sm:my-3 lg:justify-start lg:mx-4">
                         <li className="bg-isesuma-darkblue rounded-md cursor-pointer mx-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={props.amazon}><span><h3 className="2xl text-white mx-1 font-light bg-transparent">  Amazon  </h3></span></Link></li>
                         <li className="bg-isesuma-darkblue rounded-md cursor-pointer mx-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={props.bookwalker}><span><h3 className="2xl text-white mx-1 font-light bg-transparent">  Bookwalker  </h3></span></Link></li>
                     </ul>
                 </div>
             </div>
-            <div className="flex flex-col mx-auto">
+            <div className="flex flex-col">
                 <h1 className="text-white text-3xl font-extralight uppercase text-center">Check out the volumes below!</h1>
-                <ul className=" justify-center mx-auto grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-gap-3">
+                <ul className=" justify-center mx-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:grid-gap-3 2xl:grid-cols-8 xl:grid-cols-6">
                     {props.volumeList && props.volumeList.map(volume =>
-                        <li key={volume.volumeNumber}><Link href={"/" + props.manga + "/volume" + volume.volumeNumber}><span><img src={volume.imageCover} className="max-h-96 w-auto rounded-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"></img></span></Link></li>)}
+                        <li key={volume.volumeNumber}><Link href={"/" + props.manga + "/volume" + volume.volumeNumber}>
+                            <span>
+                                <img src={volume.imageCover} className="max-h-96 w-auto rounded-2xl p-0 object-contain lg:mx-auto">
+                                </img>
+                            </span></Link></li>)}
                 </ul>
             </div>
-        </Fragment>
+        </div>
     </div>
 }
 export async function getStaticPaths() {
