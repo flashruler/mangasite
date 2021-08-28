9aeb23438afdf9cd00802dedf5907dd07216a89e
flashruler <44584657+flashruler@users.noreply.github.com>
2021-08-27 18:23:53 -0700

    update

--- pages/faq.js
+++ pages/faq.js
@@ -9,10 +9,10 @@ return(
         <h1 className="text-white text-xl font-extralight uppercase text-center"> Q: What is this site?</h1>
         <h2 className="text-white text-base font-extralight uppercase text-center"> A: Its the official website for The Lost Smartphone, a new manga scanlation team</h2>
     </div>
-    <div className="my-3 mx-4">
+    {/* <div className="my-3 mx-4">
         <h1 className="text-white text-xl font-extralight uppercase text-center"> Q: Why host volumes 1-10 of smartphone?</h1>
         <h2 className="text-white text-base font-extralight uppercase text-center"> A: To keep consistency for readers so they can enjoy the full series! Additionally, we take strides to give proper credit to our predecessors by linking to their social channels</h2>
-    </div>
+    </div> */}
     <div className="my-3 mx-4">
         <h1 className="text-white text-xl font-extralight uppercase text-center"> Q: Will there be more than just smartphone?</h1>
         <h2 className="text-white text-base font-extralight uppercase text-center"> A: Eventually yes!</h2>
